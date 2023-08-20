9348b3110af9295338d603ed3b14bacbd4d09f75
abhijitttt <112143922+abhijitttt@users.noreply.github.com>
2023-08-20 06:10:01 +0600

    Add files via upload

---
+++ 1335A.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int test;
+    scanf("%i",&test);
+    int num[test];
+
+    for(int i = 0; i < test; i++)
+    {
+        scanf("%i",&num[i]);
+    }
+
+    for(int i = 0; i < test; i++)
+    {
+        if(num[i] % 2 == 0) printf("%i\n",num[i]/2-1);
+        else printf("%i\n",num[i]/2);
+    }
+}
